8dab0e67197af91050504a403803e8671151ff46
Huynh Nguyen Hieu Nhan <hieunhantv2002@gmail.com>
2022-12-25 23:04:57 -0800

    finish lab5

---
+++ code/Core/Inc/global.h
@@ -0,0 +1,60 @@
+/*
+ * global.h
+ *
+ *  Created on: Dec 25, 2022
+ *      Author: NHAN
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "main.h"
+
+#define NUMBER_OF_TIMER 																				1
+#define TIME_CYCLE																								10
+#define TIMER_UART																							0
+
+extern int state_command_parser;
+extern int state_uart_communication;
+extern uint32_t ADC_value;
+
+extern int RST_flag;
+extern int OK_flag;
+
+
+
+//data in terminal is not in forme "!XXXXXX#"
+#define NO_RECEIVE_DATA 																					0
+// get "!"
+#define START																										1
+
+
+//get "RST"
+#define GET_R																										2
+#define GET_S																										3
+#define GET_T																										4
+//
+#define END_RST																									5
+
+
+////get "OK"
+#define GET_O																										6
+#define GET_K																										7
+#define END_OK																									8
+
+
+
+
+
+//
+
+#define RST_CMD				 																					9
+#define OK_CMD
+
+
+
+
+
+
+
+#endif /* INC_GLOBAL_H_ */
---
+++ code/Core/Src/global.c
@@ -0,0 +1,17 @@
+/*
+ * global.c
+ *
+ *  Created on: Dec 25, 2022
+ *      Author: NHAN
+ */
+
+
+#include "global.h"
+
+int state_command_parser = NO_RECEIVE_DATA;
+//int state_uart_communication =
+
+int RST_flag = 0;
+int OK_flag = 0;
+
+uint32_t ADC_value = 0;
